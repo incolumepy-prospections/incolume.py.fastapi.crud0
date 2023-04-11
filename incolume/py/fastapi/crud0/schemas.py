dd12e3ad9f27fc3257b913242d854aad5e29295f
britodfbr <britodfbr@gmail.com>
2023-04-11 12:01:51 -0300

    feat: User Schemas

---
+++ incolume/py/fastapi/crud0/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from config import settings
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+    @validator('username')
+    def check_username(cls, value):
+        if not re.match(settings.REGEX_USERNAME, value):
+            raise ValueError('Invalide format for username')
+        return value
+    
+    class Config:
+        orm_mode = True
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    pw_hash: str
