c862f0b174acf8ce67c3bb138d16765fef860ba1
britodfbr <britodfbr@gmail.com>
2023-04-12 08:29:50 -0300

    feat: Endpoint users disponibilizado no servidor

--- incolume/py/fastapi/crud0/server.py
+++ incolume/py/fastapi/crud0/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from config import settings
-
+from incolume.py.fastapi.crud0.routers import auth, user
 
 app = FastAPI()
 
@@ -8,3 +8,6 @@ app = FastAPI()
 @app.get("/", include_in_schema=False)
 async def root():
     return {"message": settings.msg}
+
+
+app.include_router(user.router)
