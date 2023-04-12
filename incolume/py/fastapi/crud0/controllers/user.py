859dc0c495313c80c783a45e9214866d8145e411
britodfbr <britodfbr@gmail.com>
2023-04-12 08:26:53 -0300

    feat: implemenatação dos metodos all e one; e Ajustes no create

--- incolume/py/fastapi/crud0/controllers/user.py
+++ incolume/py/fastapi/crud0/controllers/user.py
@@ -5,7 +5,7 @@ from fastapi import status
 from fastapi.exceptions import HTTPException
 
 from incolume.py.fastapi.crud0.models import UserModel
-from incolume.py.fastapi.crud0.schemas import UserIn
+from incolume.py.fastapi.crud0.schemas import UserIn, UserInDB
 
 
 crypt_context = CryptContext(schemes=['sha256_crypt'])
@@ -15,25 +15,29 @@ class User:
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-    def create(self, user: UserIn):
-        user_model = UserModel(
-            **user,
-            pw_hash=crypt_context.hash(user.password)
-        )
+    def create(self, user: UserIn) -> UserModel:
+        hash=crypt_context.hash(user.password)
+        del user.password
+        new_user = UserInDB(**user.dict(), pw_hash=hash)
+        user_model = UserModel(**new_user.dict())
         try:
             self.db_session.add(user_model)
             self.db_session.commit()
         except IntegrityError:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists.')
+        return user_model
         
-    def update(self):
+    def update(self, user_id: int) -> UserModel:
         pass
 
-    def delete(self):
+    def delete(self) -> UserModel:
         pass
 
-    def all(self):
-        pass
+    def all(self) -> list[UserModel]:
+        users = self.db_session.query(UserModel).all()
+        return users
 
-    def one(self):
-        pass
+    def one(self, user_id: int) -> UserModel:
+        stmt = select(UserModel).filter_by(id=user_id)
+        user = self.db.execute(stmt).one()
+        return user
