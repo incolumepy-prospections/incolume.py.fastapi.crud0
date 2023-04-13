66122082b07ad6d09a699cd054647ce41b306eaf
britodfbr <britodfbr@gmail.com>
2023-04-13 08:39:00 -0300

    feat: 'DB' Refinamento de configurações conforme documentação oficial em https://fastapi.tiangolo.com/tutorial/sql-databases/

--- incolume/py/fastapi/crud0/db/connections.py
+++ incolume/py/fastapi/crud0/db/connections.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from config import settings
 
 Base = declarative_base()
-engine = create_engine(settings.db_url, pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
+engine = create_engine(settings.db_url, pool_pre_ping=True, connect_args={"check_same_thread": False})
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
 def get_db_session():
