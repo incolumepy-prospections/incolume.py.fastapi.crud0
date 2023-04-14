7eecad4857ed3c802c6f666402cdb8748d0e5dd2
Brito <britodfbr@users.noreply.github.com>
2023-04-13 22:20:13 -0300

    Atualizar o conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from typing import Generator
+from fastapi.testclient import TestClient
+from incolume.py.fastapi.crud0.server import app 
+from incolume.py.fastapi.crud0.db.persistence import recreate_db 
+
+@pytest.fixture(scope="function")
+def client() -> Generator:
+    recreate_db()
+    with TestClient(app) as cliente:
+        yield cliente
