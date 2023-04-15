760ea9a26585125ad212649a42cfccbfe228591d
Brito <britodfbr@users.noreply.github.com>
2023-04-15 09:23:33 -0300

    feat: Metainformação sobre API
    Acrescentado lable para endpoints users; 
    Mepeado as variáveis de ambiente para Metainformações;
    personalização de endpoint para prefix/tags no carregamento do server;

--- incolume/py/fastapi/crud0/server.py
+++ incolume/py/fastapi/crud0/server.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 from config import settings
+from incolume.py.fastapi.crud0 import __version__
 from incolume.py.fastapi.crud0.routers import auth, user
 from incolume.py.fastapi.crud0.db.persistence import create_db, recreate_db, populate_db
-from config import settings
+
 
 recreate_db()
 populate_db(100)
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.api_title,
+    version=__version__,
+    description=settings.api_description,
+    openapi_tags=settings.api_openapi_tags,
+    contact=settings.api_contact,
+    docs_url=settings.api_docs_url,
+    redoc_url=settings.api_redoc_url,
+)
 
 
 @app.get("/", include_in_schema=False)
@@ -15,4 +24,4 @@ async def root():
     return {"message": settings.msg}
 
 
-app.include_router(user.router)
+app.include_router(user.router, prefix="/users", tags=["Users"],)
