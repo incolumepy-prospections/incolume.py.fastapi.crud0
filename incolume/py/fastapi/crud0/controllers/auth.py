0577fa4e742edd110d8ea73cfcaebada75327c95
britodfbr <britodfbr@gmail.com>
2023-04-18 14:40:31 -0300

    feat: implementado registro de logging para class Auth

--- incolume/py/fastapi/crud0/controllers/auth.py
+++ incolume/py/fastapi/crud0/controllers/auth.py
@@ -20,18 +20,23 @@ class Auth:
         self.db_session = db_session
         
     def login(self, user:UserLogin, seconds: int = 30, minutes: int = 0, hours: int = 0, days: int = 0, weeks: int = 0):
+        logging.debug(f'{user=}, {seconds=}, {minutes=}, {hours=}, {days=}, {weeks=}')
 
         user_login = self.db_session.query(UserModel).filter_by(username=user.username).first()
         if not user_login or not crypt_context.verify(secret=user.password, hash=user_login.pw_hash):
             raise HTTPException(status_code=status.HTTP_401_UNAUTORIZED, detail='Invalid username or password')
+        
+        logging.debug(f"{user_login.__dict__=}")
         
         exp = datetime.utcnow() + timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days, weeks=weeks)
         payload = {
             'sub': user.username,
             'exp': exp,
         }
+        logging.debug(f'{payload=}')
 
         access_token = jwt.encode(payload, settings.secret_key, algorithm=settings.ALGORITHM)
+        logging.debug(f'{access_token=}')
 
         return {
             'access_token': access_token,
