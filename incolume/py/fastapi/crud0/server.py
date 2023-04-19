9e02f315cc72012d43af5f33dcdb1b1d721900bd
britodfbr <britodfbr@gmail.com>
2023-04-19 17:08:51 -0300

    feat: Redirecionamento favicon; rota par /auth/otp

--- incolume/py/fastapi/crud0/server.py
+++ incolume/py/fastapi/crud0/server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from config import settings
 from incolume.py.fastapi.crud0 import __version__
-from incolume.py.fastapi.crud0.routers import auth, user
+from incolume.py.fastapi.crud0.routers import auth, user, auth_otp
 from incolume.py.fastapi.crud0.db.persistence import create_db, recreate_db, populate_db
 
 
@@ -29,5 +29,11 @@ async def redirect_pydantic():
     return "/docs"
 
 
+@app.get("/favicon", response_class=RedirectResponse, status_code=308, include_in_schema=False)
+async def redirect_pydantic():
+    return "/auth/otp/favicon"
+
+
 app.include_router(user.router, prefix="/users", tags=["Users"],)
 app.include_router(auth.router, prefix="/auth", tags=["Auth"],)
+app.include_router(auth_otp.router, prefix="/auth/otp", tags=["Auth OTP"],)
