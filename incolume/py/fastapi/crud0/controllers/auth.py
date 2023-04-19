19318d5f67d28533f7e673a7e957a903aa857e44
britodfbr <britodfbr@gmail.com>
2023-04-19 17:09:48 -0300

    feat: Controller para auth otp

--- incolume/py/fastapi/crud0/controllers/auth.py
+++ incolume/py/fastapi/crud0/controllers/auth.py
@@ -66,3 +66,12 @@ class Auth:
 
     def generate_token(self):
         pass
+
+
+class AuthOTP:
+    def __init__(self, db_session: Session) -> None:
+        self.db_session = db_session
+
+    def login(self):
+        pass
+    
