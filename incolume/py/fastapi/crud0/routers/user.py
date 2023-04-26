f5bf8322f0bb1e8985cf73144d126fb9abee4e28
britodfbr <britodfbr@gmail.com>
2023-04-26 13:02:27 -0300

    docs: Descrição de ação acrescentado ao sumary das rotas

--- incolume/py/fastapi/crud0/routers/user.py
+++ incolume/py/fastapi/crud0/routers/user.py
@@ -9,23 +9,23 @@ from functools import singledispatch
 router = APIRouter()
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut, summary="Create an User")
 def signin(user: schemas.UserIn, session: Session = Depends(get_db_session)):
     new_user = User(session).create(user)
     return new_user
 
 
-@router.get('/', status_code=status.HTTP_202_ACCEPTED, response_model=list[schemas.UserOut])
+@router.get('/', status_code=status.HTTP_202_ACCEPTED, response_model=list[schemas.UserOut], summary="List all users")
 def list_users(skip:int = 0, limit=10, session: Session = Depends(get_db_session)):
     return User(session).all(skip=skip, limit=limit)
 
 
-@router.get('/{user_id: int}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.UserOut)
+@router.get('/{user_id: int}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.UserOut, summary="List an user by id")
 def get_user(user_id: int, db: Session = Depends(get_db_session)):
     user = User(db).one(user_id)
     return user
 
-@router.get('/{username_or_email: str}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.UserOut)
+@router.get('/{username_or_email: str}', status_code=status.HTTP_202_ACCEPTED, response_model=schemas.UserOut, summary="List an user by username or email")
 def get_user_by_username_or_email(username_or_email: str, db: Session = Depends(get_db_session)):
     user = User(db).by_email(username_or_email) or User(db).by_username(username_or_email)
     return user
@@ -47,13 +47,13 @@ def get_user_by_username_or_email(username_or_email: str, db: Session = Depends(
 #     return user
 
 
-@router.put('/{user_id}', status_code=status.HTTP_202_ACCEPTED)
+@router.put('/{user_id}', status_code=status.HTTP_202_ACCEPTED, summary="Update data for user by id")
 def update_user(user_id: int, db: Session = Depends(get_db_session)):
     user = User(db).update(user_id)
     return user
 
 
-@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT, summary="Delete an user by id")
 def delete_user(user_id: int, db: Session = Depends(get_db_session)):
     user = User(db).delete(user_id)
     return user
