8ad348a01c274670961780d87bc6789dda4d1dff
britodfbr <britodfbr@gmail.com>
2023-05-05 10:42:15 -0300

    docs: Elucidação dos perfis/papéis de atuação

--- incolume/py/fastapi/crud0/controllers/utils.py
+++ incolume/py/fastapi/crud0/controllers/utils.py
@@ -2,17 +2,17 @@ from enum import Enum, IntFlag, auto
 
 
 class Role(IntFlag):
-    # Sem previlégios pública
+    # Sem previlégios - status de criação default (somente acesso público)
     USER = 0
-    # leitor
+    # leitor - poderá ler versões anteriores dos itens
     READER = auto()
-    # editor
+    # editor - poderá ler/editar versões anteriores dos itens
     EDITOR = auto()
-    # Revisor
+    # Revisor - poderá ler/atualizar items
     PROOFREADER = auto()
-    # Gestor/Gerente
+    # Gestor/Gerente - poderá gerenciar/conceder privilégios dos perfis e todas as permissões anteriores 
     MANAGER = auto()
-    # Administrador
+    # Administrador - poderá gerenciar/conceder privilégios dos perfis e todas as permissões anteriores 
     # ADMINISTRATOR = USER | READER | EDITOR | PROOFREADER | MANAGER
     ADMINISTRATOR = auto()
 
