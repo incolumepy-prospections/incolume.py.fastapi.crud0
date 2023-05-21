053ddb1a14da2a1c559fa51880d8fd2612fc4369
Britodfbr <britodfbr@gmail.com>
2023-05-21 07:48:38 -0300

    refactor: Alterado DB do exemplo para sqlite

--- examples/sumanshu-nankana/database.py
+++ examples/sumanshu-nankana/database.py
@@ -5,12 +5,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from typing import Generator
 
 # For PostgreSQL Database
-SQLALCHEMY_DATABASE_URL = settings.DB_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = settings.DB_URL
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # For SQLite Database
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./sqlite.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread" : False})
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./sqlite.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- examples/sumanshu-nankana/routers/users.py
+++ examples/sumanshu-nankana/routers/users.py
@@ -10,7 +10,8 @@ router = APIRouter()
 
 @router.post("/user", tags=["user"], response_model=ShowUser)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    user = User(email=user.email, password=Hasher.get_hash_password(user.password))
+    user = User(email=user.email,
+                password=Hasher.get_hash_password(user.password))
     db.add(user)
     db.commit()
     db.refresh(user)
